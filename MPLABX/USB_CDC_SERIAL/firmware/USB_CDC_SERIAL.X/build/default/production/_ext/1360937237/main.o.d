76449652084fc3621370eb91c3923f5b4900598f
clockworks001 <nag03412@nifty.ne.jp>
2016-08-18 23:24:45 +0900

    second commit

--- MPLABX/USB_CDC_SERIAL/firmware/USB_CDC_SERIAL.X/build/default/production/_ext/1360937237/main.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1360937237/main.o:  \
-../src/main.c  \
- ../../../../../../../../Microchip/harmony/v1_07_01/framework/system/common/sys_module.h  \
- ../../../../../../../../Microchip/harmony/v1_07_01/framework/system/common/sys_common.h 
